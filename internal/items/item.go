c3d5e389b4cb77c4b30634e8b445c3a8c6e262f9
Sam Clark <samrocksc@gmail.com>
2025-11-02 14:04:18 +0100

    feat: Add items as JSON files with schema support
    - Create JSON files for items with proper schema references
    - Add functions to load items from JSON files
    - Update rooms to include sample items (sword, potion, shield)
    - Modify items package to support JSON loading
    - Add sample items to starting rooms

--- internal/items/item.go
+++ internal/items/item.go
@@ -1,5 +1,11 @@
 package items
 
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+)
+
 // Item represents an item in the game
 type Item struct {
 	Name        string
@@ -24,4 +30,45 @@ type ItemStats struct {
 	Strength     int
 	Intelligence int
 	Dexterity    int
+}
+
+// LoadItemFromJSON loads an item from a JSON file
+func LoadItemFromJSON(filename string) (*Item, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	var item Item
+	if err := json.Unmarshal(data, &item); err != nil {
+		return nil, err
+	}
+
+	return &item, nil
+}
+
+// LoadAllItemsFromDirectory loads all items from JSON files in a directory
+func LoadAllItemsFromDirectory(directory string) (map[string]*Item, error) {
+	items := make(map[string]*Item)
+	
+	files, err := os.ReadDir(directory)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, file := range files {
+		if filepath.Ext(file.Name()) == ".json" {
+			filename := filepath.Join(directory, file.Name())
+			item, err := LoadItemFromJSON(filename)
+			if err != nil {
+				return nil, err
+			}
+			
+			// Use the item name as the key (converted to lowercase and spaces replaced with underscores)
+			key := file.Name()[:len(file.Name())-5] // Remove .json extension
+			items[key] = item
+		}
+	}
+
+	return items, nil
 }
